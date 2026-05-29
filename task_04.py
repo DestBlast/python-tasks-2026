7b4db796d84dabd5959d20464503beb1bbcb9025
DestBlast <68483344+DestBlast@users.noreply.github.com>
2026-05-29 15:08:57 +0300

    Create task_04.py

---
+++ task_04.py
@@ -0,0 +1,11 @@
+name = "John"
+group = "A1"
+age = 20
+city = "NY"
+
+print("+----------------------------+")
+print(f"| NAME : {name}".ljust(29) + "|")
+print(f"| GROUP: {group}".ljust(29) + "|")
+print(f"| AGE  : {age}".ljust(29) + "|")
+print(f"| CITY : {city}".ljust(29) + "|")
+print("+----------------------------+")
